bbafb728b73aea53f6a9c46fe964c294c6a974c0
Resolas <64108044+Resolas@users.noreply.github.com>
2020-09-25 12:12:13 +0100

    Basic Unit Targeting and Fog Of War

---
+++ StrategyPrototype/Assets/_Scripts/Shoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public GameObject projectile;
+    public float fireRate;
+    public float coolDown;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        coolDown += coolDown * Time.deltaTime;
+        if (coolDown > 1 / fireRate)
+        {
+           // coolDown = 
+        }
+    }
+
+    
+
+}
